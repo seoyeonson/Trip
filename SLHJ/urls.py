3af3a53832d1a158e19afb07d03dc9458710dd05
SoyoungHeo2 <tblskt@naver.com>
2022-03-28 14:11:13 +0900

    허소영#3.28/14:07
    hotel_reserve.html 작성
    vacation_reserve.html 작성
    hotel 과 vacation 예약페이지는 css 거의 동일합니다. css 파일에 /*reserve*/ 로 표시해두었습니다.

--- SLHJ/urls.py
+++ SLHJ/urls.py
@@ -3,4 +3,7 @@ from SLHJ import views
 
 urlpatterns = [
     path('main/', views.main),
+    path('hotel_reserve/', views.hotel_reserve),
+    path('vacation_reserve/', views.vacation_reserve),
+
 ]
--- SLHJ/views.py
+++ SLHJ/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 def main(request):
     return render(request, 'main.html')
 
+def hotel_reserve(request):
+    return render(request, 'hotel_reserve.html')
+
+def vacation_reserve(request):
+    return render(request, 'vacation_reserve.html')
