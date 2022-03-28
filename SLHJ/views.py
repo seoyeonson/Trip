373a61f1aca1e6acd6bdd9a6c32eb341d2ed45c6
seoyeonson <eo970721@gmail.com>
2022-03-28 16:03:53 +0900

    손서연#3.28/16:02
    user.html, user_create.html, hotel_detail.html 수정
    
    base.html에 header 추가

--- SLHJ/views.py
+++ SLHJ/views.py
@@ -3,8 +3,17 @@ from django.shortcuts import render
 def main(request):
     return render(request, 'main.html')
 
+def user(request):
+    return render(request, 'user.html')
+
+def user_create(request):
+    return render(request, 'user_create.html')
+
 def hotel_reserve(request):
     return render(request, 'hotel_reserve.html')
 
 def vacation_reserve(request):
     return render(request, 'vacation_reserve.html')
+
+def hotel_detail(request):
+    return render(request, 'hotel_detail.html')
