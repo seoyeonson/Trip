4639d4a3a121c2782ffa58e8eee3777cfb412330
gwo0o <102335174+gwo0o@users.noreply.github.com>
2022-03-29 16:06:23 +0900

    이광우#3.29/16:05
    main.html 위치, 체크인, 및 form 수정

--- static/bs/js/js.js
+++ static/bs/js/js.js
@@ -1,9 +1,73 @@
 function search_hotel() {
     document.getElementById("hotel_search").style.display ='block';
     document.getElementById("vacation_search").style.display ='none';
+    document.getElementById("vacation_location").style.display = 'none';
 }
 
 function search_vacation() {
     document.getElementById("hotel_search").style.display ='none';
     document.getElementById("vacation_search").style.display ='block';
+    document.getElementById("hotel_location").style.display = 'none';
+    document.getElementById("date_pick").style.display = 'none';
 }
+
+function search_location() {
+    document.getElementById("hotel_location").style.display ='block';
+}
+
+function hotel_lo_select() {
+    document.getElementById("hotel_location").style.display ='none';
+}
+
+function search_va_location() {
+    document.getElementById("vacation_location").style.display ='block';
+}
+
+function vacation_lo_select() {
+    document.getElementById("vacation_location").style.display ='none';
+}
+
+function get_date() {
+    document.getElementById("date_pick").style.display ='block';
+}
+
+function input_date() {
+    start_day = document.querySelector("#start_day").value.slice(5,10);
+    end_day = document.querySelector("#end_day").value.slice(5,10);
+    document.getElementById("date_pick").style.display = 'none';
+}
+
+// $(function() {
+//     $('input[name="daterange"]').daterangepicker({
+//       opens: 'left'
+//     }, function(start_day, end_day, label) {
+//       console.log(start_day.format('MM-DD') + ' to ' + end_day.format('MM-DD'));
+//     });
+// });
+
+$(document).ready(function() {
+    $("#hotel_lo_select1").click(function() {
+        $("#lo_text").html("서울");
+    });
+    $("#hotel_lo_select2").click(function() {
+        $("#lo_text").html("경기");
+    });
+    $("#hotel_lo_select3").click(function() {
+        $("#lo_text").html("인천");
+    });
+
+    $("#vacation_lo_select1").click(function() {
+        $("#va_lo_text").html("서울");
+    });
+    $("#vacation_lo_select2").click(function() {
+        $("#va_lo_text").html("경기");
+    });
+    $("#vacation_lo_select3").click(function() {
+        $("#va_lo_text").html("인천");
+    });
+
+    $("#submit_day").click(function() {
+        $("#chk_in").html(start_day);
+        $("#chk_out").html(end_day);
+    });
+});
