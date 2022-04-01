d4e77ddda96a3172b8bb7ce9bd489a493d0ce333
gwo0o <102335174+gwo0o@users.noreply.github.com>
2022-04-01 16:33:05 +0900

    이광우#4.1/16:32
    views.py sample . sample data를 입력받기위한 포맷입니다.

--- SLHJ/urls.py
+++ SLHJ/urls.py
@@ -25,6 +25,9 @@ urlpatterns = [
     path('history_vacation/', views.history_vacation, name='history_vacation'),
 
     #api data용
-    path('api/', views.api, name='api'),
-    path('api2/', views.api2, name='api2')
+    # path('api/', views.api, name='api'),
+    # path('api2/', views.api2, name='api2')
+    path('sample/', views.sample, name='sample'),   # vacation_review data 입력포맷
+    path('sample2/', views.sample2, name='sample2') # vacation_reserve data 입력포맷
+
 ]
