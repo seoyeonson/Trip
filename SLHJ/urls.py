194ce5a35139f82f0bc89c63a2927194856aa85b
seoyeonson <eo970721@gmail.com>
2022-04-01 13:48:25 +0900

    손서연#4.01/13:45
    login.html, user_create.html, nav 수정
    user_create.css 추가, js.js(로그인, 회원가입 버튼기능 추가)

--- SLHJ/urls.py
+++ SLHJ/urls.py
@@ -1,10 +1,11 @@
+from unicodedata import name
 from django.urls import path
 from SLHJ import views
 
 urlpatterns = [
-    path('main/', views.main),
+    path('main/', views.main, name='main'),
     path('list/', views.list),
-    path('login/', views.login),
+    path('login/', views.login, name='login'),
     path('user_create/', views.user_create),
     path('hotel_reserve/', views.hotel_reserve),
     path('vacation_reserve/', views.vacation_reserve),
