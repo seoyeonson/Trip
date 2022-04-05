d2da96e9531931899323c9cd985c030764f731b7
SoyoungHeo2 <tblskt@naver.com>
2022-04-05 16:33:51 +0900

    허소영#04.05 16:33
    휴대폰 인증 기능 구현 방법
    아직 여러가지 시도중입니다.

--- SLHJ/phone_confirm.py
+++ SLHJ/phone_confirm.py
@@ -1,31 +1,50 @@
-import sys, random, math
+import sys, random, math, requests, json
+from selenium import webdriver
 from sdk.api.message import Message
 from sdk.exceptions import CoolsmsException
+from bs4 import BeautifulSoup as bs
 
-if __name__ == "__main__":
-    # set api key, api secret
-    api_key = "NCSBBQKQMGO7AFBL"
-    api_secret = "AUB8IHWTNNKQEYV7DTAETK8BFTTOJVMS"
-    ## 4 params(to, from, type, text) are mandatory. must be filled
-    params = dict()
-    params['type'] = 'sms' # Message type ( sms, lms, mms, ata )
-    params['to'] = '01000000000' # Recipients Number '01000000000,01000000001'
-    params['from'] = '01000000000' # Sender number
-    auth_num = math.floor(random.random()*(10**6))
-    params['text'] = f'''Trip에서 전송된 인증번호 입니다.
-    인증번호 : {auth_num}
-    '''
-    cool = Message(api_key, api_secret)
-    try:
-        response = cool.send(params)
-        print("Success Count : %s" % response['success_count'])
-        print("Error Count : %s" % response['error_count'])
-        print("Group ID : %s" % response['group_id'])
-        if "error_list" in response:
-            print("Error List : %s" % response['error_list'])
-    except CoolsmsException as e:
-        print("Error Code : %s" % e.code)
-        print("Error Message : %s" % e.msg)
+# # def coolSMS(phoneNum):
+# url = 'http://127.0.0.1:8000/vacation_reserve/'
+# try:
+#     res = requests.get(url)
+#     soup = bs(res.content, 'html.parser')
 
-    sys.exit()
+#     attr = soup.find(id="coolsms_pNum")
+#     print("HTML:/n", attr)
+# except:
+#     print("Invalid URL or some error occured while making the GET request to the specified URL")
+
+wd = webdriver.Chrome(executable_path='C:/Users/tblsk/OneDrive/문서/2022_KDigital/DevRoot/Proj/TeamProj1/SLHJ/chromedriver')
+url = 'http://127.0.0.1:8000/vacation_reserve/'
+result = wd.get(url)
+print(result)
+
+
+# if __name__ == "__main__":
+#     # set api key, api secret
+#     api_key = "NCSBBQKQMGO7AFBL"
+#     api_secret = "AUB8IHWTNNKQEYV7DTAETK8BFTTOJVMS"
+#     ## 4 params(to, from, type, text) are mandatory. must be filled
+#     params = dict()
+#     params['type'] = 'sms' # Message type ( sms, lms, mms, ata )
+#     params['to'] = 'phoneNum' # Recipients Number '01000000000,01000000001'
+#     params['from'] = '01034544451' # Sender number
+#     auth_num = math.floor(random.random()*(10**6))
+#     params['text'] = f'''Trip에서 전송된 인증번호 입니다.
+#     인증번호 : {auth_num}
+#     '''
+#     cool = Message(api_key, api_secret)
+#     try:
+#         response = cool.send(params)
+#         print("Success Count : %s" % response['success_count'])
+#         print("Error Count : %s" % response['error_count'])
+#         print("Group ID : %s" % response['group_id'])
+#         if "error_list" in response:
+#             print("Error List : %s" % response['error_list'])
+#     except CoolsmsException as e:
+#         print("Error Code : %s" % e.code)
+#         print("Error Message : %s" % e.msg)
+
+#     sys.exit()
 
