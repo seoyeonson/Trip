1bd1584226b434da6ba2e82f519b72050c14b080
SoyoungHeo2 <tblskt@naver.com>
2022-04-12 16:31:52 +0900

    허소영#04.12 16:31
    휴대폰 검증 수정

--- SLHJ/phone_confirm.py
+++ SLHJ/phone_confirm.py
@@ -1,53 +1,33 @@
-import sys, random, math, requests, json
-# from selenium import webdriver
+import sys, random, math, requests, json, sdk
 from sdk.api.message import Message
 from sdk.exceptions import CoolsmsException
 from bs4 import BeautifulSoup as bs
 
-page = requests.get("https://library.gabia.com/")
-soup = bs(page.text, "html.parser")
+##  @brief This sample code demonstrate how to send sms through CoolSMS Rest API PHP
+if __name__ == "__main__":
+    # set api key, api secret
+    api_key = "NCSBBQKQMGO7AFBL"
+    api_secret = "AUB8IHWTNNKQEYV7DTAETK8BFTTOJVMS"
 
-elements = soup.select('div.esg-entry-content a > span')
+    ## 4 params(to, from, type, text) are mandatory. must be filled
+    params = dict()
+    params['type'] = 'sms' # Message type ( sms, lms, mms, ata )
+    params['to'] = '01034544451' # Recipients Number '01000000000,01000000001'
+    params['from'] = '01034544451' # Sender number
+    params['text'] = 'Test Message' # Message
 
-for index, element in enumerate(elements, 1):
-		print("{} 번째 게시글의 제목: {}".format(index, element.text))
-
-
-def coolSMS(phoneNum):
-    url = 'http://127.0.0.1:8000/vacation_reserve/'
+    cool = Message(api_key, api_secret)
     try:
-        res = requests.get(url)
-        soup = bs(res.content, 'html.parser')
+        response = cool.send(params)
+        print("Success Count : %s" % response['success_count'])
+        print("Error Count : %s" % response['error_count'])
+        print("Group ID : %s" % response['group_id'])
 
-        attr = soup.find(id="coolsms_pNum")
-        print("HTML:/n", attr)
-    except:
-        print("Invalid URL or some error occured while making the GET request to the specified URL")
+        if "error_list" in response:
+            print("Error List : %s" % response['error_list'])
 
-    if __name__ == "__main__":
-        # set api key, api secret
-        api_key = "NCSBBQKQMGO7AFBL"
-        api_secret = "AUB8IHWTNNKQEYV7DTAETK8BFTTOJVMS"
-        ## 4 params(to, from, type, text) are mandatory. must be filled
-        params = dict()
-        params['type'] = 'sms' # Message type ( sms, lms, mms, ata )
-        params['to'] = 'phoneNum' # Recipients Number '01000000000,01000000001'
-        params['from'] = '01034544451' # Sender number
-        auth_num = math.floor(random.random()*(10**6))
-        params['text'] = f'''Trip에서 전송된 인증번호 입니다.
-        인증번호 : {auth_num}
-        '''
-        cool = Message(api_key, api_secret)
-        try:
-            response = cool.send(params)
-            print("Success Count : %s" % response['success_count'])
-            print("Error Count : %s" % response['error_count'])
-            print("Group ID : %s" % response['group_id'])
-            if "error_list" in response:
-                print("Error List : %s" % response['error_list'])
-        except CoolsmsException as e:
-            print("Error Code : %s" % e.code)
-            print("Error Message : %s" % e.msg)
+    except CoolsmsException as e:
+        print("Error Code : %s" % e.code)
+        print("Error Message : %s" % e.msg)
 
-        sys.exit()
-
+    sys.exit()
